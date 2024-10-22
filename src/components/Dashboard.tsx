ddbdc911618dca3ed5a927892ecc6721e3897e2a
pavan-pani <pavanpk1012@gmail.com>
2024-10-22 10:58:51 +0530

    dashboard componet

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { HiOutlineUserCircle } from "react-icons/hi";
-import Sidebar from "./Sidebar";
-import ProfileSection from "./Profile";
+const ProfileSection = React.lazy(() => import('./Profile'));
+const Sidebar = React.lazy(() => import('./Sidebar'));
+import orders from '../utility/orders.json'; 
 
 const Dashboard: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -15,20 +16,51 @@ const Dashboard: React.FC = () => {
   const toggleProfile = () => {
     setShowProfile((prev) => !prev); 
   };
+  
 
   return (
     <>
       <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
       <ProfileSection show={showProfile} position="top" />
-      <div className="p-4 md:ml-64 overflow-auto w-screen">
-        <div className="flex justify-between items-center">
-          <button className="md:hidden" onClick={toggleSidebar}>
-            <RxHamburgerMenu size={20} />
+      <div className="py-3 px-8 md:ml-64 overflow-auto w-screen bg-[#303030]">
+        <div className="flex justify-between items-center rounded-sm">
+          <button aria-label="Hamburger Menu" className="md:hidden" onClick={toggleSidebar}>
+            <RxHamburgerMenu size={20} color="#fff"/>
           </button>
-          <button className="md:hidden" onClick={toggleProfile}>
-            <HiOutlineUserCircle size={30} />
+          <button aria-label="user" className="md:hidden" onClick={toggleProfile}>
+            <HiOutlineUserCircle size={30} color="#fff"/>
           </button>
         </div>
+        <div className="flex justify-between items-center mt-4">
+          <span className="text-xl font-bold text-white">Orders</span>
+          <button className="p-1 px-3 text-center text-white rounded-md bg-[#494747]">Create</button>
+        </div>
+        <div className="container mx-auto py-4">
+      <div className="overflow-x-auto">
+        <table className="min-w-full text-[#f5f5f5e1]  ">
+          <thead>
+            <tr className=" text-left border-b-[1px] border-[#ffffff30]">
+              <th className="cell">Order Number</th>
+              <th className="cell">Purchase Date</th>
+              <th className="cell">Customer</th>
+              <th className="cell">Event</th>
+              <th className="cell text-right">Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order, index) => (
+              <tr key={index} className=" border-b-[1px] border-[#ffffff30]">
+                <td className="cell">{order.orderNumber}</td>
+                <td className="cell text-[#ffffff6f]">{order.date}</td>
+                <td className="cell">{order.customer}</td>
+                <td className="cell">{order.event}</td>
+                <td className="cell text-right">{order.amount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
       </div>
     </>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { CiHome, CiShoppingCart, CiSettings } from "react-icons/ci";
 import { HiDocumentDuplicate, HiUserCircle, HiSparkles, HiQuestionMarkCircle } from "react-icons/hi";
 import { HiOutlineChevronUp, HiOutlineChevronDown, HiOutlineX } from "react-icons/hi";
-import ProfileSection from "./Profile";
+const ProfileSection = React.lazy(() => import('./Profile'));
 
 const Sidebar: React.FC<{ isOpen: boolean; toggleSidebar: () => void }> = ({ isOpen, toggleSidebar }) => {
   const [showProfile, setShowProfile] = useState(false);
@@ -16,19 +16,19 @@ const Sidebar: React.FC<{ isOpen: boolean; toggleSidebar: () => void }> = ({ isO
   return (
     <>
       <nav className={`fixed z-30 inset-y-0 left-0 w-64 flex flex-col justify-between bg-black text-white transition-transform transform ${isOpen ? "translate-x-0" : "-translate-x-full"} md:translate-x-0`}>
-        <button onClick={toggleSidebar} className="md:hidden p-2 fixed top-1 right-2 pb-5">
+        <button aria-label="close" onClick={toggleSidebar} className="md:hidden p-2 fixed top-1 right-2 pb-5">
           <HiOutlineX />
         </button>
         <div>
           <div className="text-lg flex justify-between mt-4 md:mt-0 font-bold p-4">
             <span>Neo UI</span>
-            <button onClick={() => setNavItems(!showNavItems)}>
+            <button aria-label="show/hide" onClick={() => setNavItems(!showNavItems)}>
               {showNavItems ? <HiOutlineChevronDown /> : <HiOutlineChevronUp />}
             </button>
           </div>
           <div className="h-[1px] bg-gray-500"></div>
           {showNavItems && (
-            <div className="mt-4 px-4">
+            <ul className="mt-4 px-4">
               <li className="li-icon">
                 <CiHome className="size-5" />
                 <span>Home</span>
@@ -45,18 +45,18 @@ const Sidebar: React.FC<{ isOpen: boolean; toggleSidebar: () => void }> = ({ isO
                 <CiSettings className="size-5" />
                 <span>Settings</span>
               </li>
-            </div>
+            </ul>
           )}
           <div className="p-4 text-zinc-500">Upcoming events</div>
-          <div className="px-4 flex flex-col gap-2">
-            <li className="block">Bear Hug: Live in Concert</li>
-            <li className="block">Six Fingers -- DJ set</li>
-            <li className="block">We All Look Some</li>
-            <li className="block">Viking People</li>
-          </div>
+          <ul className="px-4 flex flex-col gap-2">
+            <li className="list">Bear Hug: Live in Concert</li>
+            <li className="list">Six Fingers -- DJ set</li>
+            <li className="list">We All Look Some</li>
+            <li className="list">Viking People</li>
+          </ul>
         </div>
         <div>
-          <div className="px-4">
+          <ul className="px-4">
             <li className="li-icon">
               <HiQuestionMarkCircle className="size-5" />
               <span>Support</span>
@@ -64,9 +64,9 @@ const Sidebar: React.FC<{ isOpen: boolean; toggleSidebar: () => void }> = ({ isO
             <li className="li-icon">
               <HiSparkles className="size-5" /> <span>Changelog</span>
             </li>
-          </div>
+          </ul>
           <div className="h-[1px] my-2 bg-gray-500"></div>
-          <div onClick={toggleProfile} className="cursor-pointer hidden md:block">
+          <button aria-label="profile" onClick={toggleProfile} className="cursor-pointer hidden md:block">
             <div className="flex pl-2">
               <HiUserCircle className="size-12" />
               <div className="flex flex-col">
@@ -74,7 +74,7 @@ const Sidebar: React.FC<{ isOpen: boolean; toggleSidebar: () => void }> = ({ isO
                 <span className="text-gray-500 text-xs">goudara.pavan.kumar@gmail.com</span>
               </div>
             </div>
-          </div>
+          </button>
         </div>
       </nav>
       <ProfileSection show={showProfile} position="bottom" />
